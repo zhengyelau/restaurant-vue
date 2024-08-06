d5a1f547925e2caf4c131d9f36b20d16a6ccea79
Zheng Ye <zhengyelau@hotmail.com>
2024-08-06 22:30:12 +0800

    User login logic

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
+import { ref } from "vue";
+import Home from "./Home.vue";
+import router from "../router";
+import axios from "axios";
 
+const email = ref('')
+const password = ref('');
+
+const login = async() => {
+    // console.log("email: " + email.value + "\npassword: " + password.value)
+    const res = await axios.get(`http://localhost:3000/user?email=${email.value}&password=${password.value}`);
+    if (res.status === 200 && res.data.length > 0) {
+        localStorage.setItem('userInfo', JSON.stringify(res))
+        router.push({ path: '/', component: Home })
+    } else {
+        alert('Invalid user')
+    }
+}
 </script>
 
 
@@ -11,13 +28,11 @@
     <div class="login">
         <input type="text" v-model="email" placeholder="Enter Email" />
         <input type="text" v-model="password" placeholder="Enter Password" />
-        <button>Login</button>
+        <button @click="login">Login</button>
         <p>
             <RouterLink to="/sign-up">Sign up</RouterLink>
         </p>
     </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
